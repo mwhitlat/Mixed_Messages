df4ea71759cfa12575871ab433d35a2517c18035
Matt Whitlatch <33441863+mwhitlat@users.noreply.github.com>
2021-11-30 19:28:51 -0600

    add technology & launch to README and exclamations to message generator

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 # Mixed Messages 
 A program to return a random response using JS
-## Typical Golf Sayings
-I'm a fan of golf- and I've ***codified*** several of the responses I say to myself or others during a round.
 
-> Examples include:
-> + "Crushed that drive"
-> + "Sunk that putt"
-> + "Hooked that shot"
+## Technologies
+Javasript
+Node
 
-### Code description:
+## Code description:
 The code is pretty basic, and is outlined in these steps:
 + create arrays to pull words from (shots, desciptors, pronouns)
 + create variables to hold random indicies of each array
 + create 'message' variable to pull it all together
 
+## Launch
+To run the code, execute the following command in terminal:
+"node mixed_messages.js"
 
--- mixed_messages.js
+++ mixed_messages.js
@@ -1,18 +1,20 @@
 // Create arrays of words to be drawn on
 
 let shots = ['drive', 'chip', 'putt']
-let descriptors = ['crushed','hooked', 'pushed', 'pulled', 'drew']
+let descriptors = ['crushed','hooked', 'pushed', 'pulled', 'drew', 'bladed', 'sliced']
 let pronouns = ['Matt', 'Joel', 'Tyler', 'Brandon', 'I', 'You']
 let identifiers = ['that']
+let exclamations = ['Dang!', 'Wow!', 'Yikes,']
 // Create random functions for each array
 
 const randomShots = Math.floor(Math.random() * shots.length);
 const randomDescriptors = Math.floor(Math.random() * descriptors.length);
 const randomPronouns = Math.floor(Math.random() * pronouns.length);
 const randomIdentifiers = Math.floor(Math.random() * identifiers.length);
+const randomExclamations = Math.floor(Math.random() * exclamations.length);
 
 // pull it all together in a message variable 
-let message = pronouns[randomPronouns] + " " + descriptors[randomDescriptors] + " " + identifiers[randomIdentifiers] + " " +  shots[randomShots] 
+let message = `${exclamations[randomExclamations]} ${pronouns[randomPronouns]} ${descriptors[randomDescriptors]} ${identifiers[randomIdentifiers]} ${shots[randomShots]}.` 
 
 // print the variable
 console.log(message)
