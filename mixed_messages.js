bd5340240e07bf399016650ac6b7ffca9085fac3
Matt Whitlatch <33441863+mwhitlat@users.noreply.github.com>
2021-11-30 19:00:09 -0600

    add mixed message code

--- mixed_messages.js
+++ mixed_messages.js
@@ -4,3 +4,12 @@ let shots = ['drive', 'chip', 'putt']
 let descriptors = ['crushed','hooked', 'pushed', 'pulled', 'drew']
 let pronouns = ['Matt', 'Joel', 'Tyler', 'Brandon', 'I', 'You']
 let identifiers = ['that']
+
+const randomShots = Math.floor(Math.random() * shots.length);
+const randomDescriptors = Math.floor(Math.random() * descriptors.length);
+const randomPronouns = Math.floor(Math.random() * pronouns.length);
+const randomIdentifiers = Math.floor(Math.random() * identifiers.length);
+
+let message = pronouns[randomPronouns] + " " + descriptors[randomDescriptors] + " " + identifiers[randomIdentifiers] + " " +  shots[randomShots] 
+
+console.log(message)
