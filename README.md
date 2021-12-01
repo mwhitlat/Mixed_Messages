2401e108b24dd80714483df79d3629ca6efa16fb
Matt Whitlatch <33441863+mwhitlat@users.noreply.github.com>
2021-11-30 19:09:06 -0600

    add commentary

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # Mixed Messages 
 A program to return a random response using JS
-## Typical Golf Messages
+## Typical Golf Sayings
 I'm a fan of golf- and I've ***codified*** several of the responses I say to myself or others during a round.
 
 > Examples include:
 > + "Crushed that drive"
 > + "Sunk that putt"
 > + "Hooked that shot"
-# Mixed_Messages
+
+### Code description:
+The code is pretty basic, and is outlined in these steps:
++ create arrays to pull words from (shots, desciptors, pronouns)
++ create variables to hold random indicies of each array
++ create 'message' variable to pull it all together
+
+
--- mixed_messages.js
+++ mixed_messages.js
@@ -4,12 +4,15 @@ let shots = ['drive', 'chip', 'putt']
 let descriptors = ['crushed','hooked', 'pushed', 'pulled', 'drew']
 let pronouns = ['Matt', 'Joel', 'Tyler', 'Brandon', 'I', 'You']
 let identifiers = ['that']
+// Create random functions for each array
 
 const randomShots = Math.floor(Math.random() * shots.length);
 const randomDescriptors = Math.floor(Math.random() * descriptors.length);
 const randomPronouns = Math.floor(Math.random() * pronouns.length);
 const randomIdentifiers = Math.floor(Math.random() * identifiers.length);
 
+// pull it all together in a message variable 
 let message = pronouns[randomPronouns] + " " + descriptors[randomDescriptors] + " " + identifiers[randomIdentifiers] + " " +  shots[randomShots] 
 
+// print the variable
 console.log(message)
